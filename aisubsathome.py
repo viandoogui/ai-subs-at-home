cbfdd31df2fb1fb5cc5d3b0b3d11c8a4224710cc
Anton Lu <100625705+antonlu1029@users.noreply.github.com>
2025-09-09 15:07:28 -0500

    automatically rename example output to video title and move it to the specified folder

--- aisubsathome.py
+++ aisubsathome.py
@@ -1,11 +1,13 @@
 from yt_dlp import YoutubeDL
 import os
+import shutil
 
 class Downloader:
     def __init__(self):
         self.token = None
         self.yt = YoutubeDL({"postprocessors":[{"key":"FFmpegExtractAudio"}],"outtmpl":"%(title)s.%(ext)s", "format":"bestaudio"})
-        self.video_link = "https://www.youtube.com/watch?v=AFQ_m3Cz08o"
+        self.video_link = ""
+        self.subs_dir = os.path.abspath("")
 
     def generate_subs(self, filename):
         #you can paste the python api code from another hugging face space (using gradio) here then modify the parameters
@@ -17,7 +19,8 @@ class Downloader:
 		target_lang="Japanese",
 		api_name="/subtitle_maker")
         #this will vary depending on what the output is from above
-        print(os.path.normpath(result[0]))
+        output_path = os.path.normpath(result[0])
+        shutil.move(output_path, self.subs_dir + "/" + filename + ".srt")
         os.remove(filename + ".opus")
 
     def main(self):
