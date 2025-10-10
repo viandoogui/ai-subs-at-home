32bc5beda5e9af7e5c12fa9c509f5c497647d0e2
Anton Lu <100625705+antonlu1029@users.noreply.github.com>
2025-10-10 17:47:17 -0500

    fix invalid filename issue and allow for more languages

--- aisubsathome.py
+++ aisubsathome.py
@@ -5,33 +5,41 @@ import shutil
 class Downloader:
     def __init__(self):
         self.token = "" #make a hugging face account, generate an access token, then paste it here
-        self.yt = YoutubeDL({"postprocessors":[{"key":"FFmpegExtractAudio"}],"outtmpl":"%(title)s.%(ext)s", "format":"bestaudio"})
-        self.video_link = "" 
+        self.yt = YoutubeDL({"postprocessors":[{"key":"FFmpegExtractAudio"}], "outtmpl":"%(title)s.%(ext)s", "format":"bestaudio", "progress_hooks":[self.name_hook]})
         self.subs_dir = os.path.abspath("C:/Users/bob/Downloads/subtitles/") #put your own output directory here, with forward slashes
+        self.vid_link = "" 
+        self.vid_title = ""
+
+    def name_hook(self, data):
+        self.vid_title, ext = os.path.splitext(data["info_dict"]["_filename"])
 
     def set_link(self, link):
-        self.video_link = link
+        self.vid_link = link
 
     def generate_subs(self, filename):
         from gradio_client import Client, handle_file
         #each free hf user can use 3 free minutes of ZeroGPU a day with this space
         #the site tells you 4, but 1 is blocked out at runtime by default, since computing may or may not finish
         #as long as you stay under 3 out of 4 minutes of the day (check!), it should work fine
-        client = Client("viandoogui/kotoba-japanese-srt", hf_token=self.token)
+        client = Client("viandoogui/japanese-srt", hf_token=self.token)
+        
+        #use this instead for languages other than Japanese (uses more ZeroGPU)
+        #client = Client("viandoogui/whisper-srt", hf_token=self.token)
+        
         result = client.predict(
                 audio=handle_file(os.path.normpath(os.path.join(os.path.dirname(__file__), filename + ".opus"))),
                 task="transcribe",
                 api_name="/test"
         )
+        
         print(result)
         output_path = os.path.normpath(result[1])
         shutil.move(output_path, self.subs_dir + "/" + filename + ".srt")
         os.remove(filename + ".opus")
 
     def main(self):
-        info = self.yt.extract_info(self.video_link)
-        filename = info["title"]
-        self.generate_subs(filename)
+        self.yt.download(self.vid_link)
+        self.generate_subs(self.vid_title)
 
 d = Downloader()
 
