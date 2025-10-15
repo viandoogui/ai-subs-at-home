25ae7a6a1ef3333cfb8aa6c002715f88efd3f230
Anton Lu <100625705+antonlu1029@users.noreply.github.com>
2025-10-15 18:02:13 -0500

    ditch hugging face for google colab

--- aisubsathome.py
+++ aisubsathome.py
@@ -1,12 +1,13 @@
 from yt_dlp import YoutubeDL
 import os
 import shutil
+from gradio_client import Client, handle_file
 
 class Downloader:
     def __init__(self):
-        self.token = "" #make a hugging face account, generate an access token, then paste it here
+        self.gradio_link = "" 
         self.yt = YoutubeDL({"postprocessors":[{"key":"FFmpegExtractAudio"}], "outtmpl":"%(title)s.%(ext)s", "format":"bestaudio", "progress_hooks":[self.name_hook]})
-        self.subs_dir = os.path.abspath("C:/Users/bob/Downloads/subtitles/") #put your own output directory here, with forward slashes
+        self.subs_dir = os.path.abspath("C:/Users/anton/Downloads/subtitles/testing") #put your own output directory here, with forward slashes
         self.vid_link = "" 
         self.vid_title = ""
 
@@ -17,31 +18,31 @@ class Downloader:
         self.vid_link = link
 
     def generate_subs(self, filename):
-        from gradio_client import Client, handle_file
-        #each free hf user can use 3 free minutes of ZeroGPU a day with this space
-        #the site tells you 4, but 1 is blocked out at runtime by default, since computing may or may not finish
-        #as long as you stay under 3 out of 4 minutes of the day (check!), it should work fine
-        client = Client("viandoogui/japanese-srt", hf_token=self.token)
-        
-        #use this instead for languages other than Japanese (uses more ZeroGPU)
-        #client = Client("viandoogui/whisper-srt", hf_token=self.token)
-        
+        client = Client(self.gradio_link)
         result = client.predict(
-                audio=handle_file(os.path.normpath(os.path.join(os.path.dirname(__file__), filename + ".opus"))),
-                task="transcribe",
-                api_name="/test"
+        media_file=handle_file(os.path.normpath(os.path.join(os.path.dirname(__file__), filename + ".opus"))),
+        source_lang="Japanese",
+        target_lang="Japanese",
+        api_name="/subtitle_maker"
         )
-        
         print(result)
-        output_path = os.path.normpath(result[1])
+
+        output_path = os.path.normpath(result[0])
         shutil.move(output_path, self.subs_dir + "/" + filename + ".srt")
         os.remove(filename + ".opus")
+        client.close()
 
     def main(self):
-        self.yt.download(self.vid_link)
+        try:
+            self.yt.download(self.vid_link)
+        except:
+            print("Download failed, try again or update yt-dlp using 'pip install yt-dlp --upgrade'")
+            pass
         self.generate_subs(self.vid_title)
-
+        
 d = Downloader()
+
+d.gradio_link = input("Enter the current gradio link: ")
 
 while True:
     link = input("Enter a Youtube link: ")
